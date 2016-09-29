e7dee443fd15224fab524fc2ddb36a7cddedd7a6
Kudrat Kurban <kurtkurban@gmail.com>
2016-09-29 15:27:33 -0400

    for loop practice

---
+++ list.py
@@ -0,0 +1,91 @@
+place_to_vist = ['span','japen','germani','guljia']
+
+print(place_to_vist)
+
+print(sorted(place_to_vist))
+print(place_to_vist)
+print(sorted(place_to_vist, reverse=True))
+print(place_to_vist)
+place_to_vist.reverse()
+print(place_to_vist)
+place_to_vist.reverse()
+print(place_to_vist)
+place_to_vist.sort()
+print(place_to_vist)
+place_to_vist.sort(reverse=True)
+print(place_to_vist)
+
+
+#=======================================
+list_try = ['d', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'j', 'k', 'l']
+print(sorted(list_try, reverse=True))
+print(list_try)
+# print(sorted(list_try))
+# print(list_try)
+removed = list_try.pop()
+print(removed)
+list_try.remove('t')
+print(list_try)
+del list_try[0]
+print(list_try)
+print(len(list_try))
+list_try.reverse()
+print(list_try)
+list_try.insert(0, 'x')
+print(list_try)
+
+pizzas = ['bbq chken', 'pesto chicken', 'italian']
+
+friend_pizza =
+
+for pizza in pizzas:
+    print("I like " + pizza)
+
+print("pizza is good")
+
+animals = ['cat', 'dog', 'fish']
+for animal in animals:
+    print("A " + animal + "  would make great pet.")
+
+message = "Call me at 202-999-3647"
+
+
+#=============================================================
+
+guest_list = ['sabit','kaysar','mirxiat','dilmirat']
+
+print(len(guest_list))
+
+print(guest_list[3].title()+ " can't come.")
+
+not_coming = guest_list.remove('dilmirat')
+guest_list.insert(0,'arafat')
+guest_list.insert(3,'komoiraon')
+guest_list.append ('alim')
+
+print ("monday night my place for dinner "+guest_list[0].title())
+print ("monday night my place for dinner "+guest_list[1].title())
+print ("monday night my place for dinner "+guest_list[2].title())
+print ("monday night my place for dinner "+guest_list[3].title())
+print ("monday night my place for dinner "+guest_list[4].title())
+print ("monday night my place for dinner "+guest_list[5].title())
+
+
+remove_gust = guest_list.pop(0)
+print("sorry i can not invite you to the dinner due to my new table haven't arrive "+ remove_gust)
+remove_gust = guest_list.pop()
+print("sorry i can not invite you to the dinner due to my new table haven't arrive "+ remove_gust)
+remove_gust = guest_list.pop()
+print("sorry i can not invite you to the dinner due to my new table haven't arrive "+ remove_gust)
+remove_gust = guest_list.pop()
+print("sorry i can not invite you to the dinner due to my new table haven't arrive "+ remove_gust)
+
+print (guest_list[0] + " and "+guest_list[1] +" you two still con come for dinner.")
+
+del remove_gust[0]
+del remove_gust[1]
+print (guest_list)
+
+
+
+#=================================================
