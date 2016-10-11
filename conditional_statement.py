2a8a1795d5e012bd1eca53a5e36129bde4f1ff67
Kudrat Kurban <kurtkurban@gmail.com>
2016-10-11 14:24:26 -0400

    conditional added

---
+++ conditional_statement.py
@@ -0,0 +1,47 @@
+alien_colors = "Red"
+
+
+if alien_colors.title() == "Green":
+    print("You hva just earned 5 point.")
+elif alien_colors.title() == "Yellow":
+    print("You hva just earned 10 point.")
+elif alien_colors.title() == "Red":
+    print("You hva just earned 15 point.")
+
+
+
+
+
+
+
+#======================================================
+
+age = 3
+
+if age < 2:
+    print ("you are a baby.")
+elif age >= 2 and age <4:
+    print ("you are a toddler.")
+elif age >= 4 and age <13:
+    print("you are a kid.")
+elif age >= 13 and age <20:
+    print("you are a teenager.")
+elif age >= 20 and age <65:
+    print("you are a adult.")
+elif age > 65:
+    print("you are a toddler.")
+
+
+#===========================================================
+
+
+
+favorite_fruits = ["apple","mango","litchi"]
+
+
+if "apple" in favorite_fruits:
+    print ("you rally like apple. ")
+if "mango" in favorite_fruits:
+    print ("you rally like mango. ")
+if "litchi" in favorite_fruits:
+    print ("you rally like litchi. ")
