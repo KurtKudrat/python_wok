0311db67f161ee5d63ea8642b1bffb1d8b11ad24
Kudrat Kurban <kurtkurban@gmail.com>
2017-01-05 16:48:32 -0500

    store user input data in json file .

--- file_and_exceptions.py
+++ file_and_exceptions.py
@@ -107,7 +107,7 @@ read_txt('cat.txt')
 read_txt('dog.txt')
 
 
-'''
+
 
 #count the phrase
 
@@ -131,15 +131,81 @@ count_the_phrase('Adventures of Huckleberry Finn.txt')
 
 
 
+# store data as Json file
+import json
+
+file_name = 'favorite number.json'
+
+favorite_number = input('what is your favorite number?')
+
+with open(file_name,'w') as f_obj:
+    json.dump(favorite_number,f_obj)
+
+
+with open(file_name) as f_obj:
+    favorite_number = json.load(f_obj)
+    print("I know your favorite number is " + favorite_number )
 
 
 
 
+# put tow code together
+
+import json
+
+file_name = 'favorite number.json'
+
+try:
+    with open(file_name) as f_obj:
+        favorite_number = json.load(f_obj)
+
+except FileNotFoundError:
+
+    favorite_number = input('what is your favorite number?')
+    with open(file_name, 'w') as f_obj:
+        json.dump(favorite_number, f_obj)
+        print("we will remember you when you come back, " + favorite_number + "!")
+
+else:
+
+    print("I know your favorite number is " + favorite_number)
 
 
+'''
 
 
+import json
+
+def get_stored_username():
+
+    file_name = 'username.json'
+
+    try:
+        with open(file_name) as f_obj:
+            user_name = json.load(f_obj)
+    except FileNotFoundError:
+        return None
+    else:
+        return user_name
+
+def get_new_username():
+
+    user_name = input("what is your name?")
+    file_name = 'username.json'
+    with open(file_name,'w') as f_obj:
+        json.dump(user_name,f_obj)
+    return user_name
+
+def greet_user():
+    user_name = get_new_username()
+    if user_name:
+        correct_username = input("Is this your correct user name ? y/n")
+        if correct_username == 'y':
+            print("wellcome back " + user_name)
+
+        else:
+            user_name = get_new_username()
+            print("we will remember you when you come back, " + user_name + "!")
 
 
-
-
+greet_user()
